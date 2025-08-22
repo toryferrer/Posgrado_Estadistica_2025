62d050e238dbaaa723defd0da420ef84d3281840
toryferrer <toryferrer@live.com>
2025-08-21 20:34:24 -0600

    cargar datos

--- Ejercicio_1.R
+++ Ejercicio_1.R
@@ -57,16 +57,17 @@ write.csv(temp, "temp_final.csv")
 
 # 21/08/2025
 
-# Importar datos ----------------------------------------------------------
+# importar datos ----------------------------------------------------------
 
 # Shift+Ctrl+R para agregar una seccion
 
 # Si el csv esta en mi carpeta de proyecto, solo se ocupa leer el nombre del archivo
-datos <- read.csv("inspeccionmovilizacion.csv")
+datos <- read.csv("Act_Inv_Movilizacion.csv")
 View(datos)
 
 # Para abrir una BD desde un link 
-url <- "https://repodatos.atdt.gob.mx/api_update/senasica/actividades_inspeccion_movilizacion/29_actividades-inspeccion-movilizacion.csv" 
+url <- "https://repodatos.atdt.gob.mx/api_update/senasica/actividades_inspeccion_movilizacion/29_actividades-inspeccion-movilizacion.csv"
+
 senasica <- read.csv(url, header = T)
 # Header = True para que la primera fila la tome como las variables
 
